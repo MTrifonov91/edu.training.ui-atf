b4470f0b7c1039c31e12fb3e200f05954af329ed
mtrifonov <mihail.trifonov@endava.com>
2024-02-23 09:40:28 +0200

    Deleted commented lines

--- src/test/java/pages/CartPage.java
+++ src/test/java/pages/CartPage.java
@@ -10,12 +10,6 @@ public class CartPage extends Page {
     @FindBy(xpath = "//span[@class='title' and text()='Your Cart']")
     private WebElement pageTitle;
 
-//    @FindBy(xpath = "//input[@id='user-name']")
-//    private WebElement userNameInput;
-//
-//    @FindBy(xpath = "//input[@id='password']")
-//    private WebElement passwordInput;
-
     @FindBy(xpath = "//button[@id='checkout']")
     private WebElement checkoutButton;
 
